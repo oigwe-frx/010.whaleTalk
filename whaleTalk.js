8461a1361eaccbec41d7fae96edbfa9ec57c24e6
Osita Igwe <93604075+oigwe-frx@users.noreply.github.com>
2023-07-30 19:19:59 -0400

    Update and rename whale.js to whaleTalk.js

--- whaleTalk.js
+++ whaleTalk.js
@@ -5,19 +5,19 @@ const vowels = ['a','e','i','o','u'];
 let resultArray = [];
 
 for (let i = 0; i<input.length; i++){
-  let result = input[i].toLowerCase();
+  let inputLetter = input[i].toLowerCase();
   //console.log(i);
   for (let j = 0; j<vowels.length; j++){
     //console.log(j)
 
-    if(result === vowels[j]){
-      if(result === 'e'){
-          resultArray.push(result, 'e')
+    if(inputLetter === vowels[j]){
+      if(inputLetter === 'e'){
+          resultArray.push('e', 'e')
         }
-      else if(result === 'u'){
-        resultArray.push(result, 'u')          
+      else if(inputLetter === 'u'){
+        resultArray.push('u', 'u')          
         }
-      else resultArray.push(result)
+      else resultArray.push(inputLetter)
     }
   }
 }
